ba492e1581221fb2484b2b64e9e04b5a51c854b2
Gabriel Blanco <gabriel.blancogarcia98@gmail.com>
2023-02-14 17:50:36 +0100

    Include setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='diceware',
+    version='0.1.0',
+    author='Gabriel Blanco',
+    author_email='gabriel.blancogarcia98@gmail.com',
+    packages=['diceware'],
+    url='https://github.com/TheGBG/diceware',
+    license='LICENSE.txt',
+    description='Python implementation of diceware.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
